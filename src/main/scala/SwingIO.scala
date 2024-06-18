a29d43cf8c4e321451d0baf7519630a6d358e660
Michele Ravaioli <93596374+RavaMichi@users.noreply.github.com>
2024-06-18 22:34:16 +0200

    add builder for SwingIO, add interface SwingIO

--- src/main/scala/SwingIO.scala
+++ src/main/scala/SwingIO.scala
@@ -1,15 +1,18 @@
 import java.awt
-import java.awt.{Canvas, Dimension}
+import java.awt.{Canvas, Color, Dimension}
 import javax.swing
 import javax.swing.{JFrame, WindowConstants}
 
+trait SwingIO extends IO
+
 object SwingIO:
-  def apply(title: String, size: (Int, Int), pixelsPerUnit: Int = 100, center: (Int, Int) = (0, 0)): IO =
-    new SwingIOImpl(title, size, pixelsPerUnit, center)
-
-  private class SwingIOImpl(title: String, size: (Int, Int), pixelsPerUnit: Int, center: (Int, Int)) extends IO:
-
+  def apply(title: String, size: (Int, Int), pixelsPerUnit: Int = 100, center: (Int, Int) = (0, 0), background: Color = Color.white): SwingIO =
+    new SwingIOImpl(title, size, pixelsPerUnit, center, background)
+  
+  private class SwingIOImpl(title: String, size: (Int, Int), pixelsPerUnit: Int, center: (Int, Int), background: Color) extends SwingIO:
+    
     val canvas: Canvas = initCanvas(title, size)
+    
     override def onFrameEnd: Engine => Unit =
       engine =>
         super.onFrameEnd(engine)
@@ -36,3 +39,35 @@ object SwingIO:
       frame.setVisible(true)
       frame.setLocationRelativeTo(null)
       frame
+
+  /* builder class for SwingIO, with defaults */
+  case class SwingIOBuilder(
+                             title: String = "Title",
+                             size: (Int, Int) = (0, 0),
+                             pixelsPerUnit: Int = 100,
+                             center: (Int, Int) = (0, 0),
+                             background: Color = Color.white
+                           )
+  def withTitle(title: String): SwingIOBuilder =
+    SwingIOBuilder(title = title)
+  def withSize(size: (Int, Int)): SwingIOBuilder =
+    SwingIOBuilder(size = size)
+  def withPixelsPerUnitRatio(pixelsPerUnit: Int): SwingIOBuilder =
+    SwingIOBuilder(pixelsPerUnit = pixelsPerUnit)
+  def withCenter(center: (Int, Int)): SwingIOBuilder =
+    SwingIOBuilder(center = center)
+  def withBackgroundColor(color: Color): SwingIOBuilder =
+    SwingIOBuilder(background = color)
+    
+  extension (builder: SwingIOBuilder)
+    def build(): SwingIO = SwingIO(builder.title, builder.size, builder.pixelsPerUnit, builder.center, builder.background)
+    def withTitle(title: String): SwingIOBuilder =
+      SwingIOBuilder(title, builder.size, builder.pixelsPerUnit, builder.center, builder.background)
+    def withSize(size: (Int, Int)): SwingIOBuilder =
+      SwingIOBuilder(builder.title, size, builder.pixelsPerUnit, builder.center, builder.background)
+    def withPixelsPerUnitRatio(pixelsPerUnit: Int): SwingIOBuilder =
+      SwingIOBuilder(builder.title, builder.size, pixelsPerUnit, builder.center, builder.background)
+    def withCenter(center: (Int, Int)): SwingIOBuilder =
+      SwingIOBuilder(builder.title, builder.size, builder.pixelsPerUnit, center, builder.background)
+    def withBackgroundColor(color: Color): SwingIOBuilder =
+      SwingIOBuilder(builder.title, builder.size, builder.pixelsPerUnit, builder.center, color)
--- src/test/scala/SwingIOTest.scala
+++ src/test/scala/SwingIOTest.scala
@@ -1,11 +1,26 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
+
+import java.awt.Color
 class SwingIOTest extends AnyFlatSpec:
 
   "SwingIO" should "be an IO class" in:
     SwingIO("SwingTest", size = (500, 500)) shouldBe a [IO]
 
-  "SwingIO" should "create a GUI on creation" in :
-    SwingIO("SwingTest", size = (800, 800), pixelsPerUnit = 100, center = (0, 0))
+  it should "create a GUI on creation" in:
+    SwingIO("Swing Test", size = (800, 800), pixelsPerUnit = 100, center = (0, 0))
+    Thread.sleep(3000)
+    // test done through visual interface
+
+  it should "be fully customizable" in:
+    val frame: SwingIO =
+      SwingIO
+        .withSize((400, 400))
+        .withTitle("Swing Test")
+        .withCenter((0, 0))
+        .withPixelsPerUnitRatio(100)
+        .withBackgroundColor(Color.green)
+        .build()
+
     Thread.sleep(3000)
     // test done through visual interface
