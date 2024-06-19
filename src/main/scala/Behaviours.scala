5fffd21e50883326d155dcf53bff33a7f265d2e9
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-06-19 15:14:54 +0200

    Add scaladoc

--- src/main/scala/Behaviours.scala
+++ src/main/scala/Behaviours.scala
@@ -34,7 +34,12 @@ object Behaviours:
     def height_=(h: Double): Unit =
       if h >= 0 then this.h = h
 
-  trait Collider(private var w: Double = -1, private var h: Double = -1)
+  /** Gives the capability to detect an AABB collision to a Behaviour.
+    *
+    * @param w width of the collider, if omitted or less/equal than 0 it is equal to Positionable.width
+    * @param h height of the collider, if omitted or less/equal than 0 it is equal to Positionable.height
+    */
+  trait Collider(private var w: Double = 0, private var h: Double = 0)
       extends Positionable:
 
     dimensionable: Dimensionable =>
@@ -44,6 +49,11 @@ object Behaviours:
     def colliderWidth_=(width: Double): Unit = if width > 0 then w = width
     def colliderHeight_=(height: Double): Unit = if height > 0 then h = height
 
+    /** Detect if this Behaviour collided with another Behaviour that extends Collider
+      *
+      * @param other
+      * @return true if a collision is detected, false otherwise
+      */
     def collides(other: Collider): Boolean =
         this.y <= other.y + other.colliderHeight &&
         this.x <= other.x + other.colliderWidth &&
