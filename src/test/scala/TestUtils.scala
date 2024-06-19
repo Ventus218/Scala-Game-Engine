18773f7c64f16d13b093d1da8ff340eeec1bfc18
Alessandro Venturini <ales.ventus21@gmail.com>
2024-06-19 19:02:22 +0200

    Added a TestUtils object
    Contains common utils method for testing the engine while it's running

---
+++ src/test/scala/TestUtils.scala
@@ -0,0 +1,129 @@
+/** Utils to test Engine while it's running. These utils allow to inject a
+  * testerObject into the engine to that it can run tests in the various hooks
+  * of the engine game loop
+  */
+object TestUtils:
+  /** Testing context contains information about the test that is being run
+    *
+    * @param engine
+    * @param testerObject
+    *   the object which was injected. It may be useful to take it into account
+    *   in some tests.
+    */
+  case class TestingContext(
+      val engine: Engine,
+      val testerObject: Behaviour
+  )
+
+  import Testers.*
+  import Behaviours.NFrameStopper
+
+  extension (engine: Engine)
+    /** Runs the engine with a given scene injecting the testerObject
+      *
+      * @param scene
+      * @param testerObject
+      */
+    private def runTest(scene: Scene, testerObject: Behaviour): Unit =
+      val testScene = () => scene() ++ Seq(testerObject)
+      engine.run(testScene)
+
+    /** Runs the engine and calls `testFunction` on every EarlyUpdate
+      * @param scene
+      * @param nFramesToRun
+      *   number of frames the engine will run, defaults to 1
+      * @param testFunction
+      *   provides the current `TestingContext`
+      */
+    def testOnEarlyUpdateWithContext(scene: Scene, nFramesToRun: Int = 1)(
+        testFunction: (TestingContext) => Unit
+    ): Unit =
+      val testerObject = new EarlyUpdateTester(testFunction)
+        with NFrameStopper(nFramesToRun)
+      engine.runTest(scene, testerObject)
+
+    /** Runs the engine and calls `testFunction` on every EarlyUpdate
+      * @param scene
+      * @param nFramesToRun
+      *   number of frames the engine will run, defaults to 1
+      * @param testFunction
+      */
+    def testOnEarlyUpdate(scene: Scene, nFramesToRun: Int = 1)(
+        testFunction: => Unit
+    ): Unit =
+      engine.testOnEarlyUpdateWithContext(scene, nFramesToRun): (_) =>
+        testFunction
+
+    /** Runs the engine and calls `testFunction` on every Update
+      * @param scene
+      * @param nFramesToRun
+      *   number of frames the engine will run, defaults to 1
+      * @param testFunction
+      *   provides the current `TestingContext`
+      */
+    def testOnUpdateWithContext(scene: Scene, nFramesToRun: Int = 1)(
+        testFunction: (TestingContext) => Unit
+    ): Unit =
+      val testerObject = new UpdateTester(testFunction)
+        with NFrameStopper(nFramesToRun)
+      engine.runTest(scene, testerObject)
+
+    /** Runs the engine and calls `testFunction` on every Update
+      * @param scene
+      * @param nFramesToRun
+      *   number of frames the engine will run, defaults to 1
+      * @param testFunction
+      */
+    def testOnUpdate(scene: Scene, nFramesToRun: Int = 1)(
+        testFunction: => Unit
+    ): Unit =
+      engine.testOnUpdateWithContext(scene, nFramesToRun): (_) =>
+        testFunction
+
+    /** Runs the engine and calls `testFunction` on every LateUpdate
+      * @param scene
+      * @param nFramesToRun
+      *   number of frames the engine will run, defaults to 1
+      * @param testFunction
+      *   provides the current `TestingContext`
+      */
+    def testOnLateUpdateWithContext(scene: Scene, nFramesToRun: Int = 1)(
+        testFunction: (TestingContext) => Unit
+    ): Unit =
+      val testerObject = new LateUpdateTester(testFunction)
+        with NFrameStopper(nFramesToRun)
+      engine.runTest(scene, testerObject)
+
+    /** Runs the engine and calls `testFunction` on every LateUpdate
+      * @param scene
+      * @param nFramesToRun
+      *   number of frames the engine will run, defaults to 1
+      * @param testFunction
+      */
+    def testOnLateUpdate(scene: Scene, nFramesToRun: Int = 1)(
+        testFunction: => Unit
+    ): Unit =
+      engine.testOnLateUpdateWithContext(scene, nFramesToRun): (_) =>
+        testFunction
+
+  /** Provides multiple concrete behaviours for testing
+    */
+  object Testers:
+
+    class EarlyUpdateTester(testFunction: (TestingContext) => Unit)
+        extends Behaviour:
+      override def onEarlyUpdate: Engine => Unit = (engine) =>
+        testFunction(TestingContext(engine, this))
+        super.onEarlyUpdate(engine)
+
+    class UpdateTester(testFunction: (TestingContext) => Unit)
+        extends Behaviour:
+      override def onUpdate: Engine => Unit = (engine) =>
+        testFunction(TestingContext(engine, this))
+        super.onUpdate(engine)
+
+    class LateUpdateTester(testFunction: (TestingContext) => Unit)
+        extends Behaviour:
+      override def onLateUpdate: Engine => Unit = (engine) =>
+        testFunction(TestingContext(engine, this))
+        super.onLateUpdate(engine)
