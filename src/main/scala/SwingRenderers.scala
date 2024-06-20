2d353b16d71cf201665b863fe5977b39b2d2eb40
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2024-06-20 09:36:43 +0200

    add SwingOvalRenderer, add creation tests

--- src/main/scala/SwingRenderers.scala
+++ src/main/scala/SwingRenderers.scala
@@ -33,3 +33,19 @@ object SwingRenderers:
         val w = (rectWidth*io.pixelsPerUnit).toInt
         val h = (rectHeight*io.pixelsPerUnit).toInt
         g2d.fillRect(pos._1, pos._2, w, h)
+
+  trait SwingOvalRenderable(width: Double, height: Double, color: Color, offset: (Double, Double) = (0, 0)) extends SwingRenderable with Positionable:
+    require(width > 0 && height > 0)
+
+    var ovalWidth: Double = width
+    var ovalHeight: Double = height
+    var ovalColor: Color = color
+    var ovalOffset: (Double, Double) = offset
+
+    override def renderer: SwingIO => Graphics2D => Unit = io =>
+      g2d =>
+        g2d.setColor(ovalColor)
+        val pos = io.pixelPosition((x + ovalOffset._1 - ovalWidth/2, y + ovalOffset._2 + ovalHeight/2))
+        val w = (ovalWidth * io.pixelsPerUnit).toInt
+        val h = (ovalHeight * io.pixelsPerUnit).toInt
+        g2d.fillOval(pos._1, pos._2, w, h)
--- src/test/scala/SwingRendererTest.scala
+++ src/test/scala/SwingRendererTest.scala
@@ -17,6 +17,11 @@ object SwingRendererTest:
       with SwingRectRenderable(width, height, color, offset)
       with Positionable(position._1, position._2)
 
+  def ovalRenderer(width: Double, height: Double, color: Color, offset: (Double, Double) = (0, 0), position: (Double, Double) = (0, 0)): SwingOvalRenderable =
+    new Behaviour
+      with SwingOvalRenderable(width, height, color, offset)
+      with Positionable(position._1, position._2)
+
   @main def testSwingRendererRect(): Unit =
     // it should display a rectangle
     val rect: SwingRenderable =
@@ -26,15 +31,35 @@ object SwingRendererTest:
     frame.draw(rect.renderer(frame))
     frame.show()
 
+  @main def testSwingRendererOval(): Unit =
+    // it should display a rectangle
+    val oval: SwingRenderable =
+      ovalRenderer(width = 1.2, height = 2, color = Color.blue, offset = (0, 0), position = (0, 0))
+
+    val frame: SwingIO = io.build()
+    frame.draw(oval.renderer(frame))
+    frame.show()
+
 class SwingRendererTest extends AnyFlatSpec:
 
   "Swing Rectangle" should "have positive sizes" in:
     an [IllegalArgumentException] shouldBe thrownBy {
-      rectRenderer(width = 0, height = 1, color = Color.red, offset = (0, 0), position = (0, 0))
+      SwingRendererTest.rectRenderer(width = 0, height = 1, color = Color.red, offset = (0, 0), position = (0, 0))
     }
     an [IllegalArgumentException] shouldBe thrownBy {
-      rectRenderer(width = -4, height = 5, color = Color.red, offset = (0, 0), position = (0, 0))
+      SwingRendererTest.rectRenderer(width = -4, height = 5, color = Color.red, offset = (0, 0), position = (0, 0))
     }
     an [IllegalArgumentException] shouldBe thrownBy {
-      rectRenderer(width = 2, height = -6, color = Color.red, offset = (0, 0), position = (0, 0))
+      SwingRendererTest.rectRenderer(width = 2, height = -6, color = Color.red, offset = (0, 0), position = (0, 0))
+    }
+
+  "Swing Oval" should "have positive sizes" in :
+    an [IllegalArgumentException] shouldBe thrownBy {
+      SwingRendererTest.ovalRenderer(width = 0, height = 1, color = Color.red, offset = (0, 0), position = (0, 0))
+    }
+    an [IllegalArgumentException] shouldBe thrownBy {
+      SwingRendererTest.ovalRenderer(width = -4, height = 5, color = Color.red, offset = (0, 0), position = (0, 0))
+    }
+    an [IllegalArgumentException] shouldBe thrownBy {
+      SwingRendererTest.ovalRenderer(width = 2, height = -6, color = Color.red, offset = (0, 0), position = (0, 0))
     }
