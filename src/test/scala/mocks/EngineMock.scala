7d161f6e7b54572d09909bc98d3b38b7975dc951
Alessandro Venturini <ales.ventus21@gmail.com>
2024-06-23 18:39:02 +0200

    Create EngineMock.scala

---
+++ src/test/scala/mocks/EngineMock.scala
@@ -0,0 +1,28 @@
+import scala.reflect.TypeTest
+import Behaviours.Identifiable
+
+class EngineMock(val io: IO, val storage: Storage) extends Engine:
+
+  override def disable(gameObject: Behaviour): Unit = ???
+
+  override def find[B <: Behaviour](using
+      tt: TypeTest[Behaviour, B]
+  )(): Iterable[B] = ???
+
+  override def find[B <: Identifiable](using tt: TypeTest[Behaviour, B])(
+      id: String
+  ): Option[B] = ???
+
+  override def destroy(gameObject: Behaviour): Unit = ???
+
+  override def enable(gameObject: Behaviour): Unit = ???
+
+  override def loadScene(scene: Scene): Unit = ???
+
+  override def run(): Unit = ???
+
+  override def deltaTimeNanos: Long = ???
+
+  override def stop(): Unit = ???
+
+  override def create(gameObject: Behaviour): Unit = ???
