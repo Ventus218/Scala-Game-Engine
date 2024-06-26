178435cfcbb9cd7f278422a73bba4b7e00d1d88e
Alessandro Venturini <ales.ventus21@gmail.com>
2024-06-26 09:40:48 +0200

    Added scaladoc about SwingInputEventsAccumulator

--- src/main/scala/SwingInputEventsAccumulator.scala
+++ src/main/scala/SwingInputEventsAccumulator.scala
@@ -7,26 +7,69 @@ import SwingIO.InputButton.*
 import SwingIO.InputEvent.*
 import java.awt.event.MouseListener
 
+/** Accumulates InputEvents organising them frame by frame.
+  *
+  * onFrameEnd must be called on every frame to let the accumulator to work
+  * properly.
+  */
 class SwingInputEventsAccumulator extends KeyListener, MouseListener:
 
   private var _lastInputEventBeforeLastFrame: Map[InputButton, InputEvent] =
     Map()
+
+  /** Records the last InputEvent before the last frame.
+    *
+    * It can be viewed as the known state of a InputButton before the last frame
+    * InputEvents had happened
+    *
+    * @return
+    */
   def lastInputEventBeforeLastFrame: Map[InputButton, InputEvent] =
     _lastInputEventBeforeLastFrame
+
+  /** Records the last InputEvent before the last frame.
+    *
+    * It can be viewed as the known state of a InputButton before the last frame
+    * InputEvents had happened
+    *
+    * @return
+    */
   private def lastInputEventBeforeLastFrame_=(
       newValue: Map[InputButton, InputEvent]
   ) = _lastInputEventBeforeLastFrame = newValue
 
   private var _lastFrameInputEvents: Map[InputButton, Seq[InputEvent]] = Map()
+
+  /** A queue of all the InputEvents (grouped by InputButton) that happened
+    * during the last frame. The first event in the queue is the older one.
+    *
+    * @return
+    */
   def lastFrameInputEvents: Map[InputButton, Seq[InputEvent]] =
     _lastFrameInputEvents
+
+  /** A queue of all the InputEvents (grouped by InputButton) that happened
+    * during the last frame. The first event in the queue is the older one.
+    *
+    * @return
+    */
   private def lastFrameInputEvents_=(
       newValue: Map[InputButton, Seq[InputEvent]]
   ) =
     _lastFrameInputEvents = newValue
 
+  /** A queue of all the InputEvents (grouped by InputButton) that happened
+    * during the current frame. The first event in the queue is the older one.
+    *
+    * It's used to concurrently record input events that are firing.
+    * @return
+    */
   private var currentFrameInputEvents: Map[InputButton, Seq[InputEvent]] = Map()
 
+  /** Should be called on every frame, it updates the internal state moving all
+    * the new input into a freezed queue which can be queried safely
+    * (`lastFrameInputEvents`).
+    */
   def onFrameEnd(): Unit =
     this.synchronized:
       lastFrameInputEvents.toSeq
@@ -52,6 +95,10 @@ class SwingInputEventsAccumulator extends KeyListener, MouseListener:
   override def mousePressed(e: SwingMouseEvent): Unit = processInputEvent(e)
   override def mouseReleased(e: SwingMouseEvent): Unit = processInputEvent(e)
 
+  /** Queues a new swing input event
+    *
+    * @param e
+    */
   private def processInputEvent(e: SwingInputEvent): Unit =
     this.synchronized:
       val event = e.getID() match
