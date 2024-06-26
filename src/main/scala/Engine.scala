fc59f27d13548aa9bea6c784b1ae375de6e401f2
Alessandro Venturini <ales.ventus21@gmail.com>
2024-06-26 15:22:16 +0200

    Added deltaTimeSeconds and removed old test file

--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -129,3 +129,9 @@ object Engine:
     */
   def apply(io: IO, storage: Storage): Engine =
     new EngineImpl(io = io, storage = storage)
+
+  extension (e: Engine)
+    /** The amount of time elapsed between the last frame and the current one in
+      * seconds
+      */
+    def deltaTimeSeconds: Double = e.deltaTimeNanos / Math.pow(10, 9)
