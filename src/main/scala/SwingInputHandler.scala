426af44078ec8af0bc55e267c643d318fcca374e
Alessandro Venturini <ales.ventus21@gmail.com>
2024-06-26 09:48:01 +0200

    Added scaladoc for SwingInputHandler

--- src/main/scala/SwingInputHandler.scala
+++ src/main/scala/SwingInputHandler.scala
@@ -4,7 +4,15 @@ object SwingInputHandler:
 
   type Handler = (InputButton) => Unit
 
+  /** A behaviour which enables to specify some event handlers to fire when
+    * specific inputs are received.
+    *
+    * All the handlers that need to be fired are executed automatically during
+    * the LateUpdate phase of the game loop.
+    */
   trait SwingInputHandler extends Behaviour:
+    /** A mapping between input buttons and their handlers.
+      */
     var inputHandlers: Map[InputButton, Handler]
 
     override def onEarlyUpdate: Engine => Unit = (engine) =>
