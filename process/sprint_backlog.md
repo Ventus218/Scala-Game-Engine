63fd31e9ed0906c44ab87a1ada31071ecfb91cc3
Alessandro Venturini <ales.ventus21@gmail.com>
2024-06-28 12:01:25 +0200

    Not possible to create/destroy same object twice

--- process/sprint_backlog.md
+++ process/sprint_backlog.md
@@ -50,7 +50,7 @@
 |18|Fix: Sovrapposizione di figure in IO|Michele|||X|
 |19|Definizione priorità di rendering|Michele|||X|
 |20|Possibilità di settare un limite di FPS|Alessandro|||X|
-|21|Usare Set al posto di Seq per la creazione/distruzione di oggetti|Tutti||X||
+|21|Usare Set al posto di Seq per la creazione/distruzione di oggetti|Tutti|||X|
 |22|Impedire l'esecuzione di run quando l'engine è già partito|Tutti|||X|
 |23|Aggiungere un riferimento all'engine negli Handler di **SwingInputHandler**|Tutti|||X|
 |24|Inserire un main complesso di test|Tutti||X||
--- src/test/scala/EngineObjectCreationTests.scala
+++ src/test/scala/EngineObjectCreationTests.scala
@@ -26,23 +26,29 @@ class EngineObjectCreationTests extends AnyFlatSpec:
           engine.find[GameObjectMock]() should contain only (obj1, obj2, obj3)
         else
           engine.find[GameObjectMock]() shouldBe empty
-          engine.create(obj1),
-      onUpdate =
-        if frame <= 1 then
-          engine.find[GameObjectMock]() shouldBe empty
-          engine.create(obj2),
-      onLateUpdate =
-        if frame <= 1 then
-          engine.find[GameObjectMock]() shouldBe empty
-          engine.create(obj3),
+          engine.create(obj1)
+      ,
+      onUpdate = if frame <= 1 then
+        engine.find[GameObjectMock]() shouldBe empty
+        engine.create(obj2),
+      onLateUpdate = if frame <= 1 then
+        engine.find[GameObjectMock]() shouldBe empty
+        engine.create(obj3),
     )
 
   it should "not instantiate a game object already present in the scene" in:
     engine.testOnUpdate(scene):
-      engine.find[GameObjectMock]() should contain (obj1)
+      engine.find[GameObjectMock]() should contain(obj1)
       an[IllegalArgumentException] shouldBe thrownBy {
         engine.create(obj1)
       }
+
+  it should "throw if trying to create an object multiple times in the same frame" in:
+    engine.testOnUpdate(scene):
+      val obj = new Behaviour {}
+      engine.create(obj)
+      an[IllegalArgumentException] shouldBe thrownBy:
+        engine.create(obj)
 
   it should "invoke the method onInit on the game object" in:
     var hasCalledInit = false
@@ -54,7 +60,7 @@ class EngineObjectCreationTests extends AnyFlatSpec:
 
     hasCalledInit shouldBe true
 
-  it should "invoke the event methods in the correct order" in :
+  it should "invoke the event methods in the correct order" in:
     import LifecycleTester.*
     import LifecycleEvent.*
     val objTester: LifecycleTester =
@@ -65,30 +71,27 @@ class EngineObjectCreationTests extends AnyFlatSpec:
     objTester.happenedEvents should contain theSameElementsInOrderAs
       Seq(Init, Start, EarlyUpdate, Update, LateUpdate, Deinit)
 
-
   "destroy" should "remove a game object from the scene" in:
     engine.testOnStart(scene):
       engine.destroy(obj1)
-    engine.find[GameObjectMock]() should contain only(obj2, obj3)
+    engine.find[GameObjectMock]() should contain only (obj2, obj3)
 
   it should "not remove a game object between EarlyUpdate, Update and LateUpdate, but after" in:
     var frame = 0
     engine.testOnLifecycleEvent(scene, nFramesToRun = 2)(
       onEarlyUpdate =
         frame += 1
-        if frame > 1 then
-          engine.find[GameObjectMock]() shouldBe empty
+        if frame > 1 then engine.find[GameObjectMock]() shouldBe empty
         else
           engine.find[GameObjectMock]() should contain theSameElementsAs scene()
-          engine.destroy(obj1),
-      onUpdate =
-        if frame <= 1 then
-          engine.find[GameObjectMock]() should contain theSameElementsAs scene()
-          engine.destroy(obj2),
-      onLateUpdate =
-        if frame <= 1 then
-          engine.find[GameObjectMock]() should contain theSameElementsAs scene()
-          engine.destroy(obj3),
+          engine.destroy(obj1)
+      ,
+      onUpdate = if frame <= 1 then
+        engine.find[GameObjectMock]() should contain theSameElementsAs scene()
+        engine.destroy(obj2),
+      onLateUpdate = if frame <= 1 then
+        engine.find[GameObjectMock]() should contain theSameElementsAs scene()
+        engine.destroy(obj3),
     )
 
   it should "remove an object only if already instantiated in the scene" in:
@@ -99,6 +102,12 @@ class EngineObjectCreationTests extends AnyFlatSpec:
       an[IllegalArgumentException] shouldBe thrownBy {
         engine.destroy(new GameObjectMock() with Identifiable("1"))
       }
+
+  it should "throw if trying to destroy an object multiple times in the same frame" in:
+    engine.testOnUpdate(scene):
+      engine.destroy(obj1)
+      an[IllegalArgumentException] shouldBe thrownBy:
+        engine.destroy(obj1)
 
   it should "invoke the method onDeinit on the game object at the end of the frame" in:
     var hasCalledDeinit = false
