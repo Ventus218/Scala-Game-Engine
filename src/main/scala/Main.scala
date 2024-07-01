8e7c997b8b888fbeef52ab34e94ee43fe6944d79
Alessandro Venturini <ales.ventus21@gmail.com>
2024-07-01 12:02:56 +0200

    improved main test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,15 +4,14 @@ import Physics2D.CircleCollider
 import java.awt.Color
 import SwingIO.InputButton
 import InputButton.*
-import Dimensions2D.Positionable
-import Dimensions2D.SingleScalable
+import Dimensions2D.*
+import SwingRenderers.*
+import Physics2D.*
 
 object ComplexTest:
   extension (e: Engine) def swingIO: SwingIO = e.io.asInstanceOf[SwingIO]
 
   @main def main: Unit =
-    import Dimensions2D.*
-    import SwingRenderers.SwingSquareRenderer
 
     val io = SwingIO
       .withTitle("Test")
@@ -25,75 +24,122 @@ object ComplexTest:
       fpsLimit = 60
     )
 
-    engine.run: () =>
-      Seq(GameObject())
+    engine.run(MenuScene)
 
-    class GameObject(val movementVelocity: Double = 40)
-        extends Behaviour
-        with Positionable
-        with SwingSquareRenderer(2, Color.blue)
-        with SwingInputHandler
-        with Velocity
-        with Acceleration(accY = -100):
-      var inputHandlers: Map[InputButton, Handler] = Map(
-        D -> onMoveRight,
-        A -> onMoveLeft,
-        MouseButton1 -> onTeleport,
-        Space -> onJump.onlyWhenPressed
-      )
+  trait Velocity(var velX: Double = 0, var velY: Double = 0)
+      extends Positionable:
+    override def onUpdate: Engine => Unit = (engine) =>
+      x += velX * engine.deltaTimeSeconds
+      y += velY * engine.deltaTimeSeconds
+      super.onUpdate(engine)
 
-      private def onTeleport(input: InputButton)(engine: Engine): Unit =
-        val pointer = engine.io.asInstanceOf[SwingIO].scenePointerPosition()
-        x = pointer._1
-        y = pointer._2
-        velX = 0
+  trait Acceleration(var accX: Double = 0, var accY: Double = 0)
+      extends Velocity:
+    override def onUpdate: Engine => Unit = (engine) =>
+      velX = velX + (accX * engine.deltaTimeSeconds)
+      velY = velY + (accY * engine.deltaTimeSeconds)
+      super.onUpdate(engine)
+
+  class MenuObj extends Behaviour with SwingInputHandler:
+    var inputHandlers: Map[InputButton, Handler] = Map(
+      P -> onPlay
+    )
+
+    private def onPlay(input: InputButton)(engine: Engine): Unit =
+      engine.loadScene(GameScene)
+
+  class Obstacle(initX: Double, initY: Double, squareSide: Double)
+      extends Behaviour
+      with Positionable(initX, initY)
+      with Scalable
+      with SwingSquareRenderer(squareSide, Color.red)
+      with RectCollider(squareSide, squareSide)
+
+  class GameObject(
+      initX: Double,
+      initY: Double,
+      circleRadius: Double,
+      val movementVelocity: Double = 40
+  ) extends Behaviour
+      with Positionable(initX, initY)
+      with SingleScalable
+      with SwingCircleRenderer(circleRadius, Color.blue)
+      with CircleCollider(circleRadius)
+      with SwingInputHandler
+      with Velocity
+      with Acceleration(accY = -100):
+    var inputHandlers: Map[InputButton, Handler] = Map(
+      D -> onMoveRight,
+      A -> onMoveLeft,
+      MouseButton1 -> onTeleport,
+      Space -> onJump.onlyWhenPressed,
+      R -> onReset
+    )
+
+    private def onTeleport(input: InputButton)(engine: Engine): Unit =
+      val pointer = engine.io.asInstanceOf[SwingIO].scenePointerPosition()
+      x = pointer._1
+      y = pointer._2
+      velX = 0
+      velY = 0
+
+    private def onMoveRight(input: InputButton)(engine: Engine): Unit =
+      x += movementVelocity * engine.deltaTimeSeconds
+
+    private def onMoveLeft(input: InputButton)(engine: Engine): Unit =
+      x -= movementVelocity * engine.deltaTimeSeconds
+
+    private var jumped = false
+    private def onJump(input: InputButton)(engine: Engine): Unit =
+      jumped = true
+
+    private def onReset(input: InputButton)(engine: Engine): Unit =
+      engine
+        .find[Obstacle]()
+        .foreach(engine.enable(_))
+
+    override def onUpdate: Engine => Unit = (engine) =>
+      val io = engine.io.asInstanceOf[SwingIO]
+      val top = io.scenePosition((0, 0))._2 - shapeHeight / 2
+      val bottom = io.scenePosition(io.size)._2 + shapeHeight / 2
+      val right = io.scenePosition(io.size)._1 - shapeWidth / 2
+      val left = io.scenePosition((0, 0))._1 + shapeWidth / 2
+
+      if y > top then
+        y = top
+        velY = 0
+      if y < bottom then
+        y = bottom
         velY = 0
 
-      private def onMoveRight(input: InputButton)(engine: Engine): Unit =
-        x += movementVelocity * engine.deltaTimeSeconds
+      if x > right then
+        x = right
+        velX = 0
+      if x < left then
+        x = left
+        velX = 0
 
-      private def onMoveLeft(input: InputButton)(engine: Engine): Unit =
-        x -= movementVelocity * engine.deltaTimeSeconds
+      if jumped then
+        jumped = false
+        velY = 60
 
-      private var jumped = false
-      private def onJump(input: InputButton)(engine: Engine): Unit =
-        jumped = true
+      // collision
+      engine
+        .find[Obstacle]()
+        .foreach(o => if collides(o) then engine.disable(o))
 
-      override def onUpdate: Engine => Unit = (engine) =>
-        val io = engine.io.asInstanceOf[SwingIO]
-        val top = io.scenePosition((0, 0))._2 - shapeHeight / 2
-        val bottom = io.scenePosition(io.size)._2 + shapeHeight / 2
-        val right = io.scenePosition(io.size)._1 - shapeWidth / 2
-        val left = io.scenePosition((0, 0))._1 + shapeWidth / 2
+      super.onUpdate(engine)
 
-        if y > top then
-          y = top
-          velY = 0
-        if y < bottom then
-          y = bottom
-          velY = 0
+  object MenuScene extends Scene:
+    override def apply(): Iterable[Behaviour] =
+      Seq(MenuObj())
 
-        if x > right then
-          x = right
-          velX = 0
-        if x < left then
-          x = left
-          velX = 0
-
-        if jumped then
-          jumped = false
-          velY = 60
-
-        super.onUpdate(engine)
-
-trait Velocity(var velX: Double = 0, var velY: Double = 0) extends Positionable:
-  override def onUpdate: Engine => Unit = (engine) =>
-    x += velX * engine.deltaTimeSeconds
-    y += velY * engine.deltaTimeSeconds
-    super.onUpdate(engine)
-
-trait Acceleration(var accX: Double = 0, var accY: Double = 0) extends Velocity:
-  override def onUpdate: Engine => Unit = (engine) =>
-    velX = velX + (accX * engine.deltaTimeSeconds)
-    velY = velY + (accY * engine.deltaTimeSeconds)
-    super.onUpdate(engine)
+  object GameScene extends Scene:
+    override def apply(): Iterable[Behaviour] =
+      Seq(
+        GameObject(initX = 0, initY = -20, circleRadius = 5),
+        Obstacle(initX = 0, initY = 0, squareSide = 5),
+        Obstacle(initX = 10, initY = 20, squareSide = 25),
+        Obstacle(initX = -20, initY = -30, squareSide = 10),
+        Obstacle(initX = 40, initY = 20, squareSide = 2)
+      )
