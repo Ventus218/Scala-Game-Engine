66493219d83a4f662e0ab3068603615e95fab00e
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-07-04 14:08:27 +0200

    Add scaladoc

--- src/main/scala/Dimensions2D.scala
+++ src/main/scala/Dimensions2D.scala
@@ -62,11 +62,22 @@ object Dimensions2D:
       */
     def apply(scale: T): Boolean
 
+  /** Gives the scaling component for width and height
+    */
   trait ScalableElement:
     def scaleWidth: Double = 1
     def scaleHeight: Double = 1
 
-  trait SingleScalable(private var _size: Double = 1) extends Behaviour with ScalableElement:
+  /** Gives the capability to a Behaviour to scale based on a single value.
+    *
+    * @param _size
+    *   value of the scaling, ScalableElement.scaleWidth and
+    *   ScalableElement.scaleHeight will be equal to this. Must be greater than
+    *   zero or otherwise throws an IllegalArgumentException.
+    */
+  trait SingleScalable(private var _size: Double = 1)
+      extends Behaviour
+      with ScalableElement:
     require(_size > 0)
 
     def scale = _size
@@ -77,15 +88,28 @@ object Dimensions2D:
     override def scaleWidth: Double = scale
     override def scaleHeight: Double = scale
 
-  trait Scalable(private var _scaleX: Double = 1, private var _scaleY: Double = 1) extends Behaviour with ScalableElement:
-    require(_scaleX > 0 && _scaleY > 0)
+  /** Gives the capability to a Behaviour to scale based on two values.
+    *
+    * @param _scaleWidth
+    *   scale value for the width, must be greater than zero or otherwise throws
+    *   an IllegalArgumentException.
+    * @param _scaleHeight
+    *   scale value for the height, must be greater than zero or otherwise
+    *   throws an IllegalArgumentException.
+    */
+  trait Scalable(
+      private var _scaleWidth: Double = 1,
+      private var _scaleHeight: Double = 1
+  ) extends Behaviour
+      with ScalableElement:
+    require(_scaleWidth > 0 && _scaleHeight > 0)
 
-    override def scaleWidth: Double = _scaleX
+    override def scaleWidth: Double = _scaleWidth
     def scaleWidth_=(s: Double) =
-      require(s > 0) 
-      _scaleX = s
+      require(s > 0)
+      _scaleWidth = s
 
-    override def scaleHeight: Double = _scaleY
-    def scaleHeight_=(s: Double) = 
-      require(s > 0) 
-      _scaleY = s
+    override def scaleHeight: Double = _scaleHeight
+    def scaleHeight_=(s: Double) =
+      require(s > 0)
+      _scaleHeight = s
