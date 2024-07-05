b2515def4031a22f443c1d1146f347531dc75899
Alessandro Venturini <ales.ventus21@gmail.com>
2024-07-05 18:42:06 +0200

    physics2d package refactor

---
+++ src/main/scala/sge/core/behaviours/physics2d/Acceleration.scala
@@ -0,0 +1,17 @@
+package sge.core.behaviours.physics2d
+
+import sge.core.*
+import metrics.Vector.*
+import EngineUtils.*
+
+/** Add an acceleration to a Velocity behaviour in order to increment (or
+  * decrement) its velocity every time the onEarlyUpdate is called.
+  *
+  * @param _acceleration
+  *   vector added to velocity according to its X and Y
+  */
+trait Acceleration(var acceleration: Vector = (0, 0)) extends Velocity:
+  override def onEarlyUpdate: Engine => Unit =
+    engine =>
+      super.onEarlyUpdate(engine)
+      velocity = velocity + acceleration * engine.deltaTimeSeconds
---
+++ src/main/scala/sge/core/behaviours/physics2d/Velocity.scala
@@ -0,0 +1,18 @@
+package sge.core.behaviours.physics2d
+
+import sge.core.*
+import behaviours.dimension2d.*
+import metrics.Vector.*
+import EngineUtils.*
+
+/** Add velocity to a Positionable behaviour, in order to move it once the
+  * onUpdate is called.
+  *
+  * @param _velocity
+  *   vecto added to position according to its X and Y.
+  */
+trait Velocity(var velocity: Vector = (0, 0)) extends Positionable:
+  override def onUpdate: Engine => Unit =
+    engine =>
+      super.onUpdate(engine)
+      position = position + velocity * engine.deltaTimeSeconds
