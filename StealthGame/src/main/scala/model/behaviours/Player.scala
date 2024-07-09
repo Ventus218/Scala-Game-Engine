27c51ea7791fe2caa0e0426dadf83a65919669ea
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-07-09 11:07:20 +0200

    Change name of State file and add onMoveTop handler to player

--- StealthGame/src/main/scala/model/behaviours/Player.scala
+++ StealthGame/src/main/scala/model/behaviours/Player.scala
@@ -5,6 +5,9 @@ import sge.swing.*
 import sge.core.behaviours.dimension2d.Positionable
 import sge.core.behaviours.physics2d.RectCollider
 import sge.core.behaviours.dimension2d.Scalable
+import model.logic.DirectionState
+import model.logic.Movement
+import model.logic.Direction
 
 class Player(
     width: Double,
@@ -15,7 +18,24 @@ class Player(
     with Positionable
     with ImageRenderer("ninja.png", width, height)
     with RectCollider(width, height)
-    with Scalable(scaleWidth, scaleHeight):
+    with Scalable(scaleWidth, scaleHeight)
+    with InputHandler:
+
+  import Privates.*
+  var inputHandlers: Map[InputButton, Handler] = Map(
+    W -> onMoveTop
+  )
+
   override def onInit: Engine => Unit = engine =>
     val io = engine.io.asInstanceOf[SwingIO]
     position = position.setX(io.scenePosition(io.size).x * -1 + width)
+
+  private object Privates:
+    import Movement.*
+    import Direction.*
+
+    var movement: DirectionState = IDLE(RIGHT)
+
+    def onMoveTop(input: InputButton): Engine => Unit = engine =>
+      ???
+      
