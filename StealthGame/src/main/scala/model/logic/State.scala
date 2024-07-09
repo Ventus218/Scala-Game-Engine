41fed2915a078ec463f9ffbc3909f59e5cf04fbb
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-07-09 10:38:36 +0200

    Add turnLeft to State

--- StealthGame/src/main/scala/model/logic/State.scala
+++ StealthGame/src/main/scala/model/logic/State.scala
@@ -7,11 +7,28 @@ enum Direction:
   case RIGHT
 
 sealed trait State:
+  def turnLeft(): State
+
+trait Directionable:
   def direction: Direction
 
-enum Movement extends State:
+enum Movement extends State with Directionable:
   case IDLE(override val direction: Direction)
   case MOVE(override val direction: Direction)
   case SPRINT(override val direction: Direction)
-  
-  
+
+  import Privates.*
+  import Direction.*
+
+  override def turnLeft(): State = this match
+    case IDLE(direction)   => IDLE(getLeftDirection(direction))
+    case MOVE(direction)   => MOVE(getLeftDirection(direction))
+    case SPRINT(direction) => SPRINT(getLeftDirection(direction))
+
+  private object Privates:
+    def getLeftDirection(direction: Direction): Direction =
+      direction match
+        case TOP    => LEFT
+        case BOTTOM => RIGHT
+        case LEFT   => BOTTOM
+        case RIGHT  => TOP
--- StealthGame/src/test/scala/model/logic/MovementTests.scala
+++ StealthGame/src/test/scala/model/logic/MovementTests.scala
@@ -7,22 +7,40 @@ import Direction.*
 
 class MovementTests extends AnyFlatSpec:
   "Movement" should "have IDLE case class with direction" in:
-    val state: State = IDLE(TOP)
+    val state: Directionable = IDLE(TOP)
     state.direction shouldBe TOP
     IDLE(LEFT).direction shouldBe LEFT
     IDLE(BOTTOM).direction shouldBe BOTTOM
     IDLE(RIGHT).direction shouldBe RIGHT
 
   it should "have MOVE case class with direction" in:
-    val state: State = MOVE(TOP)
-    state.direction shouldBe TOP
+    MOVE(TOP).direction shouldBe TOP
     MOVE(LEFT).direction shouldBe LEFT
     MOVE(BOTTOM).direction shouldBe BOTTOM
     MOVE(RIGHT).direction shouldBe RIGHT
 
   it should "have SPRINT case class with direction" in:
-    val state: State = SPRINT(TOP)
-    state.direction shouldBe TOP
+    SPRINT(TOP).direction shouldBe TOP
     SPRINT(LEFT).direction shouldBe LEFT
     SPRINT(BOTTOM).direction shouldBe BOTTOM
     SPRINT(RIGHT).direction shouldBe RIGHT
+
+  it should "turn left IDLE" in:
+    val state: State = IDLE(TOP)
+    state.turnLeft() shouldBe IDLE(LEFT)
+    IDLE(LEFT).turnLeft() shouldBe IDLE(BOTTOM)
+    IDLE(BOTTOM).turnLeft() shouldBe IDLE(RIGHT)
+    IDLE(RIGHT).turnLeft() shouldBe state
+
+  it should "turn left MOVE" in:
+    MOVE(TOP).turnLeft() shouldBe MOVE(LEFT)
+    MOVE(LEFT).turnLeft() shouldBe MOVE(BOTTOM)
+    MOVE(BOTTOM).turnLeft() shouldBe MOVE(RIGHT)
+    MOVE(RIGHT).turnLeft() shouldBe MOVE(TOP)
+
+  it should "turn left SPRINT" in:
+    SPRINT(TOP).turnLeft() shouldBe SPRINT(LEFT)
+    SPRINT(LEFT).turnLeft() shouldBe SPRINT(BOTTOM)
+    SPRINT(BOTTOM).turnLeft() shouldBe SPRINT(RIGHT)
+    SPRINT(RIGHT).turnLeft() shouldBe SPRINT(TOP)
+  
