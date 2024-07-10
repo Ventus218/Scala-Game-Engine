25fee484438e97331802ccf4c62d36d1a2f4ff5c
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-07-10 10:14:58 +0200

    Add top movement to player

--- StealthGame/src/main/scala/model/behaviours/Player.scala
+++ StealthGame/src/main/scala/model/behaviours/Player.scala
@@ -5,34 +5,102 @@ import sge.swing.*
 import sge.core.behaviours.dimension2d.{Positionable, Scalable}
 import sge.core.behaviours.physics2d.RectCollider
 import model.logic.Direction
+import sge.core.behaviours.physics2d.Velocity
 
 class Player(
     width: Double,
     height: Double,
     initialDirection: Direction,
     scaleWidth: Double = 1,
-    scaleHeight: Double = 1,
+    scaleHeight: Double = 1
+)(
+    movementVelocity: Vector2D = (1, 1)
 ) extends Behaviour
     with Positionable
     with ImageRenderer("ninja.png", width, height)
     with RectCollider(width, height)
     with Scalable(scaleWidth, scaleHeight)
+    with Velocity
     with InputHandler:
 
   import Privates.*
   var inputHandlers: Map[InputButton, Handler] = Map(
-    W -> onMoveTop
+    W -> onMoveTop.onlyWhenHeld
   )
 
   override def onInit: Engine => Unit = engine =>
+    super.onInit(engine)
     val io = engine.io.asInstanceOf[SwingIO]
     position = position.setX(io.scenePosition(io.size).x * -1 + width)
 
+  override def onUpdate: Engine => Unit = engine =>
+    super.onUpdate(engine)
+    updateSpeed()
+
+  override def onLateUpdate: Engine => Unit = engine =>
+    super.onLateUpdate(engine)
+    resetSpeed()
+
   private object Privates:
-    // import Movement.IDLE
+    import Direction.*
+    import model.logic.{*, given}
+    import MovementStateImpl.*
+    import State.*
+    import Action.*
 
-    // var initialState: State = IDLE(initialDirection)
+    var movement = MovementStateImpl(initialDirection)
+    def getDirection =
+      for d <- direction
+      yield d
 
-    def onMoveTop(input: InputButton): Engine => Unit = engine =>
-      ???
-      
+    def getAction =
+      for a <- action
+      yield a
+
+    def onMoveTop(input: InputButton): Engine => Unit =
+      engine =>
+        val moveState =
+          for
+            a <- action
+            _ <- if a == SPRINT then sprint() else move()
+          yield ()
+
+        val turnTopState =
+          for
+            _ <- moveState
+            d <- direction
+            _ <- turnToTop(d)
+          yield ()
+
+        movement = turnTopState(movement)._1
+
+    def turnToTop(d: Direction): State[Movement, Direction] =
+      if d == TOP
+      then direction
+      else
+        for
+          _ <- turnLeft()
+          d <- direction
+          _ <- turnToTop(d)
+        yield d
+
+    def updateSpeed() =
+      val direction = getDirection(movement)._2
+      val action = getAction(movement)._2
+
+      velocity = direction match
+        case TOP    => (0, movementVelocity.y)
+        case BOTTOM => (0, -movementVelocity.y)
+        case LEFT   => (-movementVelocity.x, 0)
+        case RIGHT  => (movementVelocity.x, 0)
+
+      velocity = action match
+        case IDLE   => (0, 0)
+        case MOVE   => velocity
+        case SPRINT => velocity * 1.5
+
+    def resetSpeed() =
+      val stopState =
+        for _ <- stop()
+        yield ()
+      movement = stopState(movement)._1
--- StealthGame/src/main/scala/scenes/LevelOne.scala
+++ StealthGame/src/main/scala/scenes/LevelOne.scala
@@ -8,5 +8,5 @@ import model.logic.Direction
 
 object LevelOne extends Scene:
   override def apply(): Iterable[Behaviour] = Seq(
-    Player(width = 7, height = 7, Direction.RIGHT)
+    Player(width = 7, height = 7, Direction.RIGHT)(movementVelocity = (7, 7))
   )
