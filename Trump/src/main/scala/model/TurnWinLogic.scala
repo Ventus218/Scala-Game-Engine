df28543560784aa06befc57c9022bf7106ab184a
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-07-11 19:24:30 +0200

    Made TurnWinLogic package private

--- Trump/src/main/scala/model/TurnWinLogic.scala
+++ Trump/src/main/scala/model/TurnWinLogic.scala
@@ -3,7 +3,7 @@ package model
 import Field.*
 import Cards.*
 
-object TurnWinLogic:
+private[model] object TurnWinLogic:
   def turnWinner[PlayerInfo](
       field: Field[PlayerInfo],
       trump: Suit
