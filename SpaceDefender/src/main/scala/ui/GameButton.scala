3925c393fc2933a532efafa5b94cfae522ebd97d
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2024-07-11 09:32:43 +0200

    add GameButton

---
+++ SpaceDefender/src/main/scala/ui/GameButton.scala
@@ -0,0 +1,21 @@
+package ui
+
+import sge.core.*
+import behaviours.dimension2d.Positionable
+import managers.GameConstants.*
+import sge.swing.*
+import output.Text.TextStyle.Bold
+
+import java.awt.Color
+
+class GameButton(text: String, pos: Vector2D, onPress: Engine => Unit) extends Behaviour
+  with Button(
+    _buttonText = text,
+    _textSize = buttonTextSize,
+    _textColor = Color.white,
+    _textStyle = Bold,
+    _inputButtonTriggers = Set(MouseButton1)
+  )
+  with Positionable(pos)
+  with RectRenderer(buttonSize.x, buttonSize.y, buttonColor):
+  override def onButtonPressed: Engine => Unit = onPress
