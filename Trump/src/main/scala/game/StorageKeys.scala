d12410748f835fca2a0bd4c9e0281b60d755afe9
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-07-12 14:37:08 +0200

    PlayButton sets win counter to 0 for the first time

---
+++ Trump/src/main/scala/game/StorageKeys.scala
@@ -0,0 +1,5 @@
+package game
+
+object StorageKeys:
+  val p1Wins = "p1Wins"
+  val p2Wins = "p2Wins"
--- Trump/src/main/scala/game/gameobjects/PlayButton.scala
+++ Trump/src/main/scala/game/gameobjects/PlayButton.scala
@@ -2,7 +2,16 @@ package game.gameobjects
 
 import sge.core.*
 import sge.swing.*
+import game.StorageKeys
 
 class PlayButton(position: Vector2D) extends GameButton("Play", position):
   override def onButtonPressed: Engine => Unit = engine =>
+    setWinCountersIfNotSet(engine)
     engine.loadScene(game.scenes.Game)
+
+  private def setWinCountersIfNotSet(engine: Engine): Unit =
+    Seq(StorageKeys.p1Wins, StorageKeys.p1Wins).foreach(key =>
+      engine.storage.getOption[Int](key) match
+        case Some(value) => ()
+        case None        => engine.storage.set(key, 0)
+    )
