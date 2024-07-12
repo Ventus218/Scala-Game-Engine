c362ffe0bcc9abc442e8049d6e864c5d66956428
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-07-12 21:46:10 +0200

    Hand can place card

--- Trump/src/main/scala/game/gameobjects/Hand.scala
+++ Trump/src/main/scala/game/gameobjects/Hand.scala
@@ -1,9 +1,12 @@
 package game.gameobjects
 
 import sge.core.*
+import sge.core.behaviours.dimension2d.*
+import sge.swing.*
 import game.behaviours.*
 import game.*
-import sge.core.behaviours.dimension2d.*
+import model.Cards.*
+import java.awt.Color
 
 class Hand(
     val player: String,
@@ -13,39 +16,19 @@ class Hand(
 ) extends Behaviour
     with Positionable(position):
 
-  val leftCard: CardImage = new Behaviour
-    with CardImage(_card = Option.empty, _show = show)
-    with Positionable
-    with PositionFollower(
-      this,
-      positionOffset = Versor2D.left * (Values.Dimensions.Cards.width + spacing)
-    )
-    with ChangeableImageRenderer(
-      width = Values.Dimensions.Cards.width,
-      height = Values.Dimensions.Cards.height
-    )
+  val leftCard: CardImage = handCard(
+    Versor2D.left * (Values.Dimensions.Cards.width + spacing)
+  )
+  val centerCard: CardImage = handCard()
+  val rightCard: CardImage = handCard(
+    Versor2D.right * (Values.Dimensions.Cards.width + spacing)
+  )
 
-  val centerCard: CardImage = new Behaviour
-    with CardImage(_card = Option.empty, _show = show)
-    with Positionable
-    with PositionFollower(this)
-    with ChangeableImageRenderer(
-      width = Values.Dimensions.Cards.width,
-      height = Values.Dimensions.Cards.height
-    )
-
-  val rightCard: CardImage = new Behaviour
-    with CardImage(_card = Option.empty, _show = show)
-    with Positionable
-    with PositionFollower(
-      this,
-      positionOffset =
-        Versor2D.right * (Values.Dimensions.Cards.width + spacing)
-    )
-    with ChangeableImageRenderer(
-      width = Values.Dimensions.Cards.width,
-      height = Values.Dimensions.Cards.height
-    )
+  private def onCardClicked(engine: Engine, card: Card): Unit =
+    println(card)
+    engine.gameModel.playCard(card) match
+      case Right(newGame) => engine.gameModel = newGame._1
+      case Left(value)    => throw Exception(value.message)
 
   override def onInit: Engine => Unit = engine =>
     engine.create(leftCard)
@@ -65,3 +48,25 @@ class Hand(
     if rightCard.card != newRightCard then rightCard.card = newRightCard
 
     super.onUpdate(engine)
+
+  private def handCard(positionOffset: Vector2D = (0, 0)): CardImage =
+    val width = Values.Dimensions.Cards.width
+    val height = Values.Dimensions.Cards.height
+    new Behaviour
+      with CardImage(_card = Option.empty, _show = show)
+      with Positionable
+      with PositionFollower(
+        this,
+        positionOffset = positionOffset
+      )
+      with ChangeableImageRenderer(
+        width = width,
+        height = height
+      )
+      with RectRenderer(
+        width = width,
+        height = height,
+        color = Color(0, 0, 0, 0) // Transparent
+      )
+      with Button:
+      override def onButtonPressed: Engine => Unit = onCardClicked(_, card.get)
