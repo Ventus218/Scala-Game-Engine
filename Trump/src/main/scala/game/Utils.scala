31ea15fbcfb66f19359d904cb909ebcdb3fea5d6
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-07-12 21:45:37 +0200

    Fixed Utils

--- Trump/src/main/scala/game/Utils.scala
+++ Trump/src/main/scala/game/Utils.scala
@@ -9,6 +9,8 @@ object Utils:
   extension (engine: Engine)
     def gameModel: Game[String] =
       engine.find[GameModel](Values.Ids.gameModel).get.game
+    def gameModel_=(newValue: Game[String]): Unit =
+      engine.find[GameModel](Values.Ids.gameModel).get.game = newValue
 
   extension (card: Card)
     def toImagePath: String =
@@ -17,6 +19,18 @@ object Utils:
         case model.Cards.Suit.Coins  => "coins"
         case model.Cards.Suit.Clubs  => "clubs"
         case model.Cards.Suit.Swords => "swords"
-      s"cards/$suit${card.rank.power}.png"
+      val rank = card.rank match
+        case model.Cards.Rank.Ace    => 1
+        case model.Cards.Rank.Two    => 2
+        case model.Cards.Rank.Three  => 3
+        case model.Cards.Rank.Four   => 4
+        case model.Cards.Rank.Five   => 5
+        case model.Cards.Rank.Six    => 6
+        case model.Cards.Rank.Seven  => 7
+        case model.Cards.Rank.Knave  => 8
+        case model.Cards.Rank.Knight => 9
+        case model.Cards.Rank.King   => 10
+
+      s"cards/$suit$rank.png"
 
 export Utils.*
