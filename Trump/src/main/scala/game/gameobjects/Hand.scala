f5d57a83672005383cc1caa9414b9c3167d3d69d
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-07-13 09:35:02 +0200

    Revert "Fix Hand"
    This reverts commit 30c854f8abeb0671aa64e90ffa913547ab4fa916.

--- Trump/src/main/scala/game/gameobjects/Hand.scala
+++ Trump/src/main/scala/game/gameobjects/Hand.scala
@@ -8,12 +8,8 @@ import game.*
 import model.Cards.*
 import java.awt.Color
 
-enum Player:
-  case Current
-  case Next
-
 class Hand(
-    val player: Player,
+    val player: String,
     position: Vector2D = (0, 0),
     val spacing: Double = 0,
     val show: Boolean
@@ -29,7 +25,7 @@ class Hand(
   )
 
   private def onCardClicked(engine: Engine, card: Card): Unit =
-    if player == Player.Current then
+    if engine.gameModel.currentPlayer.info == player then
       engine.gameModel.playCard(card) match
         case Right(newGame) => engine.gameModel = newGame._1
         case Left(value)    => throw Exception(value.message)
@@ -41,9 +37,7 @@ class Hand(
     super.onInit(engine)
 
   override def onUpdate: Engine => Unit = engine =>
-    val playerHand = player match
-      case Player.Current => engine.gameModel.currentPlayer.hand.cards
-      case Player.Next    => engine.gameModel.nextPlayer.hand.cards
+    val playerHand = engine.gameModel.player(player).hand.cards
     val newLeftCard = playerHand.headOption
     if leftCard.card != newLeftCard then leftCard.card = newLeftCard
 
--- Trump/src/main/scala/game/scenes/Game.scala
+++ Trump/src/main/scala/game/scenes/Game.scala
@@ -9,7 +9,7 @@ object Game extends Scene:
     GameModel(Values.Ids.gameModel),
     Deck(position = (-40, 0)),
     TrumpCard(position = (-30, 0)),
-    Hand(Player.Current, (0, -20), spacing = 1, show = true),
-    Hand(Player.Next, (0, 20), spacing = 1, show = true),
+    Hand("P1", (0, -20), spacing = 1, show = true),
+    Hand("P2", (0, 20), spacing = 1, show = false)
     Field()
   )
