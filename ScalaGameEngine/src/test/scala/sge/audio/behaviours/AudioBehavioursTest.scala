00b9fe61b520a439ff6a315e93952c6a44840632
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2026-02-04 15:41:02 +0100

    fix IOMockup on stealthgame to inherit also SoundIO

---
+++ ScalaGameEngine/src/test/scala/sge/audio/behaviours/AudioBehavioursTest.scala
@@ -0,0 +1,8 @@
+package sge.audio.behaviours
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
+import sge.core.IO
+
+class SoundIOTest extends AnyFlatSpec:
+    ""
